9bfef8abf266a18b859c6e0da0c7cbb3262eba22
David VANMAK <davidvanmak+git@gmail.com>
2023-06-19 16:32:10 +0200

    [ADD] crud controllers

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { GetAllTasksController } from './controllers/get-all-tasks/get-all-tasks.controller';
+import { CreateTaskController } from './controllers/create-task/create-task.controller';
+import { UpdateTaskController } from './controllers/update-task/update-task.controller';
+import { DeleteTaskController } from './controllers/delete-task/delete-task.controller';
 
-@Module({})
+@Module({
+  controllers: [GetAllTasksController, CreateTaskController, UpdateTaskController, DeleteTaskController]
+})
 export class TaskModule {}

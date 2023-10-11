10b655a14ed804180a29dcbd80f229248d7b9114
David VANMAK <davidvanmak+git@gmail.com>
2023-10-11 19:51:46 +0200

    [ADD] cv

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,11 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const tyoeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'localhost',
+  password: 'localhost',
+  database: 'localhost',
+  database: 'localhost',
+};
